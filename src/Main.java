0ea575a9013966799fb262c5192b6a4abbf6314e
Etkalo Serhii <69040284+vainranger144@users.noreply.github.com>
2025-03-05 18:46:04 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
-        Person person1 = new Person("Олег", 22, "Лікар");
+        Person person1 = new Person("Олег", 22, PersonRole.DOCTOR, Person.MALE);
         person1.display();
-        Person person2 = new Person("Віка", 23, "Фармацевт");
+
+        Person person2 = new Person("Віка", 23, PersonRole.OTHER, Person.FEMALE);
         person2.display();
-        person2.newProfession();
-        Person person3 = new Person("Коля", 23, "Водій");
+
+        // Зміна ролі для person2 через введення користувача
+        System.out.println("Введіть нову роль для " + person2.getName() + " (STUDENT, TEACHER, DOCTOR, ENGINEER, ARTIST, OTHER):");
+        Scanner sc = new Scanner(System.in);
+        String inputRole = sc.nextLine().toUpperCase();
+        PersonRole newRole;
+        try {
+            newRole = PersonRole.valueOf(inputRole);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Невірний ввід, роль встановлена як OTHER.");
+            newRole = PersonRole.OTHER;
+        }
+        person2.setRole(newRole);
+        person2.display();
+
+        Person person3 = new Person("Коля", 23, PersonRole.ENGINEER, Person.MALE);
         person3.display();
+
+        sc.close();
     }
 }
