cd6218f558844fb0e3f0aa9565c432d69151438c
Etkalo Serhii <69040284+vainranger144@users.noreply.github.com>
2025-03-09 14:56:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("Олег", 22, PersonRole.DOCTOR, Person.MALE);
+        Person person1 = new Man("Олег", 22, PersonRole.DOCTOR);
         person1.display();
 
-        Person person2 = new Person("Віка", 23, PersonRole.OTHER, Person.FEMALE);
+        Person person2 = new Woman("Віка", 23, PersonRole.OTHER);
         person2.display();
 
-        // Введення ролі через консоль
         System.out.println("Введіть нову роль для " + person2.getName() + " (STUDENT, TEACHER, DOCTOR, ENGINEER, ARTIST, OTHER):");
         Scanner sc = new Scanner(System.in);
         String inputRole = sc.nextLine();
 
-        // Перевірка, чи введена роль є допустимою
-        PersonRole newRole = PersonRole.OTHER; // Значення за замовчуванням
-        if (inputRole.equals("STUDENT") || inputRole.equals("TEACHER") || inputRole.equals("DOCTOR") ||
-                inputRole.equals("ENGINEER") || inputRole.equals("ARTIST")) {
+        PersonRole newRole = PersonRole.OTHER;
+        try {
             newRole = PersonRole.valueOf(inputRole);
-        } else {
+        } catch (IllegalArgumentException e) {
             System.out.println("Невірний ввід, роль встановлена як OTHER.");
         }
 
         person2.setRole(newRole);
         person2.display();
 
-        Person person3 = new Person("Коля", 23, PersonRole.ENGINEER, Person.MALE);
+        Person person3 = new Man("Коля", 23, PersonRole.ENGINEER);
         person3.display();
 
         sc.close();
