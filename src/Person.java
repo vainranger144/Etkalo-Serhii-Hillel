b01f74e5096b06ebcc73b854de670b032015d9f2
Etkalo Serhii <69040284+vainranger144@users.noreply.github.com>
2025-04-02 21:50:40 +0300

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,16 @@
-public class Person implements Displayable {
-    private String name;
-    private int age;
-    private PersonRole role; // Використовуємо enum для ролі
-    private String gender;
+public abstract class Person implements Displayable {
+    protected String name;
+    protected int age;
+    protected PersonRole role;
+    protected String gender;
 
-    public static final String MALE = "чоловік";
-    public static final String FEMALE = "жінка";
-
-    public Person(String name, int age, PersonRole role, String gender) {
+    public Person(String name, int age, PersonRole role, String gender) throws InvalidUserDataException {
+        if (name == null || name.trim().isEmpty()) {
+            throw new InvalidUserDataException("Ім'я не може бути порожнім.");
+        }
+        if (age <= 0) {
+            throw new InvalidUserDataException("Вік повинен бути більше нуля.");
+        }
         this.name = name;
         this.age = age;
         this.role = role;
@@ -30,14 +33,18 @@ public class Person implements Displayable {
         return gender;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws InvalidUserDataException {
+        if (name == null || name.trim().isEmpty()) {
+            throw new InvalidUserDataException("Ім'я не може бути порожнім.");
+        }
         this.name = name;
     }
 
-    public void setAge(int age) {
-        if (age > 0) {
-            this.age = age;
+    public void setAge(int age) throws InvalidUserDataException {
+        if (age <= 0) {
+            throw new InvalidUserDataException("Вік повинен бути більше нуля.");
         }
+        this.age = age;
     }
 
     public void setRole(PersonRole role) {
@@ -48,14 +55,11 @@ public class Person implements Displayable {
         this.gender = gender;
     }
 
-    public void display() {
-        System.out.println("Ім'я: " + name + ", Вік: " + age +
-                ", Роль: " + role +
-                ", Стать: " + gender);
-    }
+  
+    public abstract void display();
 
-    @Override
+   
     public void displayInformation() {
-
+        System.out.println("Ім'я: " + name + ", Вік: " + age + ", Роль: " + role + ", Стать: " + gender);
     }
 }
